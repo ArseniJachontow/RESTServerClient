d55f584b251dc5883db7583aef264ac473e3496c
Arseni Jachontow <jachontow@web.de>
2024-12-13 21:31:57 +0100

    Added comments

--- Client/client.cpp
+++ Client/client.cpp
@@ -9,12 +9,14 @@ std::string server = "";
 std::string endpoint = "/cars/";
 std::string server_and_endpoint = "";
 
+//these arguments are being used in main() to decide which action to call
 const std::string read_all_argument = "-read_all";
 const std::string create_car_argument = "-create_car";
 const std::string read_car_argument = "-read_car";
 const std::string update_car_argument = "-update_car";
 const std::string delete_car_argument = "-delete_car";
 
+//the functions are self explanatory by their function names
 void read_all()
 {
     std::cout << "Retrieving all cars, please wait" << std::endl;
@@ -46,7 +48,7 @@ void create_car(std::string vendor, std::string model, std::string color)
     std::cout << "Creating car. " << "Vendor: " << vendor << ", " << "Model: " << model << ", " << "Color: " << color << std::endl;
 
     auto response = cpr::Post(cpr::Url{ server_and_endpoint },
-        cpr::Body{ create_car_json.dump() },
+        cpr::Body{ create_car_json.dump() /* converts it to a string */},
         cpr::Header{ { "Content-Type", "application/json" } });
 
     if (response.status_code == 200)
--- Server/server.py
+++ Server/server.py
@@ -1,28 +1,28 @@
 import os
 
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
-class CarBase(SQLModel):
+class CarBase(SQLModel): # base model for the car, all the other models derive from it
     vendor: str
     model: str
     color: str
 
 
-class Car(CarBase, table=True):
+class Car(CarBase, table=True): # the model that is being used when creating the database file, id becomes mandatory
     id: int | None = Field(default=None, primary_key=True)
 
 
-class CarCreate(CarBase):
+class CarCreate(CarBase): # no changes necessary to the base, but gets its own model to make it look better in the doc page
     pass
 
 
-class CarPublic(CarBase):
+class CarPublic(CarBase): # after a car gets inserted into the database, it gets an id
     id: int
 
 
-class CarUpdate(SQLModel):
+class CarUpdate(SQLModel): # we are only updating the color of the cars, nothing else
     color: str
 
 
@@ -32,12 +32,12 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=False, connect_args=connect_args)
 
-if not os.path.exists(sqlite_file_name):
+if not os.path.exists(sqlite_file_name): # create the database only when it is not already existing
     SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
-@app.post("/cars/", response_model=CarPublic)
+@app.post("/cars/", response_model=CarPublic) # creating a new car
 def create_car(car: CarCreate):
     with Session(engine) as session:
         db_car = Car.model_validate(car)
@@ -47,14 +47,14 @@ def create_car(car: CarCreate):
         return db_car
 
 
-@app.get("/cars/", response_model=list[CarPublic])
+@app.get("/cars/", response_model=list[CarPublic]) # retrieving all cars
 def read_cars():
     with Session(engine) as session:
         cars = session.exec(select(Car)).all()
         return cars
 
 
-@app.get("/cars/{car_id}", response_model=CarPublic)
+@app.get("/cars/{car_id}", response_model=CarPublic) # retrieving specific car by id
 def read_car(car_id: int):
     with Session(engine) as session:
         car = session.get(Car, car_id)
@@ -63,7 +63,7 @@ def read_car(car_id: int):
         return car
 
 
-@app.patch("/cars/{car_id}", response_model=CarPublic)
+@app.patch("/cars/{car_id}", response_model=CarPublic) # updating specific car by id
 def update_car(car_id: int, car: CarUpdate):
     with Session(engine) as session:
         db_car = session.get(Car, car_id)
@@ -78,7 +78,7 @@ def update_car(car_id: int, car: CarUpdate):
         return db_car
 
 
-@app.delete("/cars/{car_id}")
+@app.delete("/cars/{car_id}") # deleting specific car by id
 def delete_car(car_id: int):
     with Session(engine) as session:
         car = session.get(Car, car_id)
@@ -87,4 +87,3 @@ def delete_car(car_id: int):
         session.delete(car)
         session.commit()
         return {"OK": True}
-    
